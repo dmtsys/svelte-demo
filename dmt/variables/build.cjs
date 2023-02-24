aa0f8bd674c8b1b8d9b0bcb66341552ef152304c
David Krmpotic <david.krmpotic@gmail.com>
2023-02-24 15:24:40 +0100

    use new dmt integrate

--- dmt/variables/build.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-let settings = fs.readFileSync('dmt/settings.json', 'utf-8');
-settings = JSON.parse(settings);
-
-// this is important as it passes the app_base to the dmt installer
-console.log(settings.build);
